f27ef39f1b92c66c5a3826551bc49d5ac254cfef
Nathan Cowley <nathancowley12@gmail.com>
2020-08-03 16:10:26 -0600

    finished mvp

---
+++ index.js
@@ -0,0 +1,88 @@
+// import express
+const express = require('express');
+const shortid = require('shortid');
+
+// create a server
+const server = express();
+
+// middleware
+server.use(express.json());
+
+let users = [];
+
+server.get('/', (req, res) => {
+    res.send('Hey Hey Hey')
+})
+
+
+// POST 
+server.post('/api/users', (req, res) => {
+    const userInfo = req.body;
+    userInfo.id = shortid.generate();
+    users.push(userInfo);
+    res.status(201).json(userInfo);
+})
+
+// GET
+server.get('/api/users/:id', (req, res) => {
+    res.status(200).json(users);
+})
+
+server.get('/api/users/:id', (req, res) => {
+    const {id} = req.params;
+    const found = users.find(user => user.id === id)
+
+    try {
+        if(found) {
+            users = users.filter(user => user.id !== id);
+            res.status(200).json(found);
+        }else{
+            res.status(404).json({ message: 'The user with the specified ID does not exist.' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: 'The user information could not be retrieved.', err})
+    }
+})
+
+// UPDATE
+server.put('/api/users/:id', (req, res) => {
+    const {id} = req.params;
+    const changes = req.body;
+
+    changes.id = id;
+
+    try{
+        let found = users.find(user => user.id === id)
+        if (found){
+            Object.assign(found, changes);
+            res.status(200).json(found)
+        }else{
+            res.status(404).json({ message: 'The user with the specified ID does not exist' })
+        }
+
+    } catch (err) {
+        res.status(500).json({ message: 'The user information could not be modified.', err })
+    }
+})
+
+// DELETE
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const deleted = users.find(user => user.id === id);
+
+    try {
+        if (deleted) {
+            users = users.filter(user => user.id !== id);
+            res.status(200).json(deleted);
+        }else{
+            res.status(404).json({ message: 'The user with the specified ID does not exist.' })
+        }
+    } catch (err) {
+        res.status(500).json({ message: 'The user could not be removed', err })
+    }
+})
+
+const PORT = 5000;
+server.listen(PORT, () => {
+    console.log('listening on localhost:', PORT)
+})
