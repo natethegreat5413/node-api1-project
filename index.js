ddb3cd2432e15123c8cdde256f5e83a7c93f1286
Nathan Cowley <nathancowley12@gmail.com>
2020-08-06 13:19:51 -0600

    push for heroku

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ server.delete('/api/users/:id', (req, res) => {
     }
 })
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 server.listen(port, () => {
     console.log(`\n*** Server Running on http://localhost:${port} ***\n`)
 })
